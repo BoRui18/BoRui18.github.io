c1c9c5281d1b21fa3460b4682aab87e889fd2dfd
BoRui18 <106426537+BoRui18@users.noreply.github.com>
2022-08-15 16:55:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 function jump(){
-    block.classList.add("animate");
+    character.classList.add("animate");
 }
