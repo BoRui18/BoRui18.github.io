371a65a5ec00b2f83b6b3c71dbfc0f6b3822246a
BoRui18 <106426537+BoRui18@users.noreply.github.com>
2022-08-21 11:39:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var character = document.getElementById("character");
 //创建变量“character”，并从文件中获取id名为“character”的元素
 var block = document.getElementById("block");
 //创建变量“block”，并从文件中获取id名为“block”的元素
-function jump(){
+/*function jump(){
     if(character.classList != "animate"){
         character.classList.add("animate");
     }
     setTimeout(function(){
         character.classList.remove("animate");
     },500);
-}
+}*/
 var checkDead = setInterval(function(){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
